ebf0261620e4f2097b3fadc9f6e5d286203b0b98
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2013-01-02 12:29:19 -0200

    changed app generation script

--- lib/template/config/initializers/00_slogger.rb
+++ lib/template/config/initializers/00_slogger.rb
@@ -1,3 +1,3 @@
 require 'slogger'
 
-Bootstrap.logger = Slogger::CommonLogger.new(Bootstrap.app_name, Bootstrap.configuration[:log_level], :local7)
+Bootstrap.logger = Slogger::CommonLogger.new(Bootstrap.app_name, Bootstrap.configuration[:log_level], :local7) if Bootstrap.logger.nil?
